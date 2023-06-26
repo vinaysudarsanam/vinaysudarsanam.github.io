e4e295ab05843990abf7ae375f9afdd7f1fe418f
vinaysudarsanam <vinay.sudarsanam.2004@gmail.com>
2023-06-26 00:25:31 -0400

    First revision

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('contactme').addEventListener('click', function() {
+    var mailtoLink = 'mailto:vinay.sudarsanam.2004@gmail.com';
+    window.open(mailtoLink);
+  });
+
+  document.getElementById("projects").addEventListener("click", function() {
+    window.location.href = "projects.html";
+  });
+
+  document.getElementById("linkedin").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/vinay-sudarsanam-182696169/", '_blank');
+  });
