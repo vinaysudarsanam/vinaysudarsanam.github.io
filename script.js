b65d28879fe6e883c770c4374e588c9793a8df23
vinaysudarsanam <vinay.sudarsanam.2004@gmail.com>
2023-07-03 14:04:59 -0400

    Update script.js
    navigating between pages functionality

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-document.getElementById('contactme').addEventListener('click', function() {
-    var mailtoLink = 'mailto:vinay.sudarsanam.2004@gmail.com';
-    window.open(mailtoLink);
-  });
+// Get references to the buttons
+const homeButton = document.getElementById('home-button');
+const projectsButton = document.getElementById('projects-button');
+const linkedinButton = document.getElementById('linkedin-button');
+const contactButton = document.getElementById('contact-button');
+const nameButton = document.getElementById('name-button');
 
-  document.getElementById("projects").addEventListener("click", function() {
+// Add event listeners to the buttons
+homeButton.addEventListener('click', handleHomeClick);
+projectsButton.addEventListener('click', handleProjectsClick);
+linkedinButton.addEventListener('click', handleLinkedinClick);
+contactButton.addEventListener('click', handleContactClick);
+nameButton.addEventListener('click', handleNameClick)
+
+// Redirect to the home page
+function handleHomeClick() {
+    window.location.href = "index.html";
+}
+
+function handleProjectsClick() {
     window.location.href = "projects.html";
-  });
+}
 
-  document.getElementById("linkedin").addEventListener("click", function() {
+function handleLinkedinClick() {
     window.open("https://www.linkedin.com/in/vinay-sudarsanam-182696169/", '_blank');
-  });
+}
 
-  document.getElementById('home').addEventListener('click', function() {
-    // Redirect to the home page
-    window.location.href = 'index.html';
-  });
+function handleContactClick() {
+  var mailtoLink = 'mailto:vinay.sudarsanam.2004@gmail.com';
+  window.open(mailtoLink);
+}
+
+// Redirect to the home page
+function handleNameClick() {
+    window.location.href = "index.html";
+}
+
