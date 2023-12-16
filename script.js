1a1613f383cc1bfc84d06750a36c42351f0f18a2
vinaysudarsanam <vinay.sudarsanam.2004@gmail.com>
2023-12-15 23:55:13 -0500

    Update script.js
    Updated 12/15

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // Get references to the buttons
 const homeButton = document.getElementById('home-button');
 const projectsButton = document.getElementById('projects-button');
-const linkedinButton = document.getElementById('linkedin-button');
 const contactButton = document.getElementById('contact-button');
 const nameButton = document.getElementById('name-button');
 
 // Add event listeners to the buttons
 homeButton.addEventListener('click', handleHomeClick);
 projectsButton.addEventListener('click', handleProjectsClick);
-linkedinButton.addEventListener('click', handleLinkedinClick);
 contactButton.addEventListener('click', handleContactClick);
 nameButton.addEventListener('click', handleNameClick)
 
@@ -26,8 +24,9 @@ function handleLinkedinClick() {
 }
 
 function handleContactClick() {
-  var mailtoLink = 'mailto:vinay.sudarsanam.2004@gmail.com';
-  window.open(mailtoLink);
+    var mailtoLink = 'mailto:vinaysud@unc.edu';
+   window.open(mailtoLink);
+
 }
 
 // Redirect to the home page
